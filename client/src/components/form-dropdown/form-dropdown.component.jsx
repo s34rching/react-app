d9d5719bcb76943740b9bc899a33f8c195355570
s34rching <ivan.katsenya@gmail.com>
2021-05-04 23:56:24 +0300

    Handle click outside

--- client/src/components/form-dropdown/form-dropdown.component.jsx
+++ client/src/components/form-dropdown/form-dropdown.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import FontAwesome from 'react-fontawesome';
 import {
   DropdownWrapper,
@@ -8,6 +8,7 @@ import {
 } from './form-dropdown.styles';
 
 export const FormDropdown = ({ dropdownTitle, options }) => {
+  const node = useRef();
   const [listState, toggleList] = useState({ listTitle: dropdownTitle, isListOpen: false });
   const { listTitle, isListOpen } = listState;
 
@@ -17,35 +18,59 @@ export const FormDropdown = ({ dropdownTitle, options }) => {
     toggleList({ ...listState, listTitle: title, isListOpen: !isListOpen });
   };
 
+  const handleClickOutside = (e) => {
+    if (node.current.contains(e.target)) {
+      return;
+    }
+
+    if (isListOpen) {
+      toggleList({ ...listState, isListOpen: false });
+    }
+  };
+
+  useEffect(() => {
+    if (isListOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isListOpen]);
+
   return (
-    <DropdownWrapper>
-      <DropdownHeaderButton
-        type="button"
-        className="dd-header"
-        onClick={() => toggleList({ ...listState, isListOpen: !isListOpen })}
-      >
-        <div className="dd-header-title">{listTitle}</div>
-        {
-          isListOpen
-            ? <FontAwesome name="angle-up" />
-            : <FontAwesome name="angle-down" />
-        }
-      </DropdownHeaderButton>
-      <DropdownListContainer isListOpen={isListOpen}>
-        {
-          options.map((item) => (
-            <DropdownOptionButton
-              type="button"
-              key={item.id}
-              onClick={() => selectItem(item)}
-            >
-              {item.title}
-              {' '}
-              {item.selected && <FontAwesome name="check" />}
-            </DropdownOptionButton>
-          ))
-        }
-      </DropdownListContainer>
-    </DropdownWrapper>
+    <div ref={node}>
+      <DropdownWrapper>
+        <DropdownHeaderButton
+          type="button"
+          className="dd-header"
+          onClick={() => toggleList({ ...listState, isListOpen: !isListOpen })}
+        >
+          <div className="dd-header-title">{listTitle}</div>
+          {
+            isListOpen
+              ? <FontAwesome name="angle-up" />
+              : <FontAwesome name="angle-down" />
+          }
+        </DropdownHeaderButton>
+        <DropdownListContainer isListOpen={isListOpen}>
+          {
+            options.map((item) => (
+              <DropdownOptionButton
+                type="button"
+                key={item.id}
+                onClick={() => selectItem(item)}
+              >
+                {item.title}
+                {' '}
+                {item.selected && <FontAwesome name="check" />}
+              </DropdownOptionButton>
+            ))
+          }
+        </DropdownListContainer>
+      </DropdownWrapper>
+    </div>
   );
 };

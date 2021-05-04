05b4b599a7d7814e469a688c28556fb4085ff959
s34rching <ivan.katsenya@gmail.com>
2021-05-04 20:50:00 +0300

    Move popup to footer

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -6,17 +6,15 @@ import { ReactComponent as Logo } from '../../static/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import SearchBar from '../search/search.component';
-import ContactUsPopup from '../contact-us-popup/contact-us-popup.component';
 import {
   HeaderContainer, LogoContainer, OptionsContainer, OptionLink,
 } from './header.styles';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
-import { toggleContactUsPopup } from '../../redux/contact-us/contact-us.actions';
-import { selectContactUsHidden } from '../../redux/contact-us/contact-us.selectors';
+
 
 export const Header = ({
-  currentUser, isCartHidden, isContactUsPopupHidden, toggleContactUsPopup, signOutStart,
+  currentUser, isCartHidden, signOutStart,
 }) => (
   <HeaderContainer data-test="header">
     <LogoContainer to="/" id="site-logo">
@@ -25,7 +23,7 @@ export const Header = ({
     <OptionsContainer>
       <SearchBar />
       <OptionLink to="/shop">SHOP</OptionLink>
-      <OptionLink as="div" onClick={toggleContactUsPopup}>CONTACT</OptionLink>
+      <OptionLink to="/contact">CONTACT</OptionLink>
       {
         currentUser
           ? <OptionLink as="div" onClick={signOutStart} data-test="sign-out-button">SIGN OUT</OptionLink>
@@ -38,23 +36,16 @@ export const Header = ({
         ? null
         : <CartDropdown />
     }
-    {
-      isContactUsPopupHidden
-        ? null
-        : <ContactUsPopup />
-    }
   </HeaderContainer>
 );
 
 export const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
   isCartHidden: selectCartHidden,
-  isContactUsPopupHidden: selectContactUsHidden,
 });
 
 export const mapDispatchToProps = (dispatch) => ({
   signOutStart: () => dispatch(signOutStart()),
-  toggleContactUsPopup: () => dispatch(toggleContactUsPopup()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);

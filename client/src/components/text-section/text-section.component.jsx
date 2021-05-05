53bc79f064729a7d1df7981dfcc5ba02d4bbc302
s34rching <ivan.katsenya@gmail.com>
2021-05-05 18:17:56 +0300

    Reebok terms

--- client/src/components/text-section/text-section.component.jsx
+++ client/src/components/text-section/text-section.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { TextSectionWrapper, HeadingContainer, ContentsContainer } from './text-section.styles';
+import { TextSectionWrapper, HeadingContainer } from './text-section.styles';
 
-export const TextSection = ({ heading, contents }) => (
+export const TextSection = ({ heading, children }) => (
   <TextSectionWrapper>
     <HeadingContainer>{heading}</HeadingContainer>
-    <ContentsContainer>{contents}</ContentsContainer>
+    {children}
   </TextSectionWrapper>
 );
--- client/src/components/text-section/text-section.styles.jsx
+++ client/src/components/text-section/text-section.styles.jsx
@@ -2,18 +2,22 @@ import styled from 'styled-components';
 
 export const TextSectionWrapper = styled.div`
   width: 100%;
+  
+  & > p {
+    margin: 30px 0 30px 0;
+    padding-bottom: 22px;
+    font-size: 18px;
+    text-align: justify
+  }
 `;
 
 TextSectionWrapper.displayName = 'TextSectionWrapper';
 
-export const HeadingContainer = styled.div`
-  width: 100%;
+export const HeadingContainer = styled.h2`
+  margin: 50px 0 30px 0;
+  border-bottom: 0.5px solid #e3e1df;
+  padding-bottom: 22px;
+  font-size: 30px;
 `;
 
 HeadingContainer.displayName = 'HeadingContainer';
-
-export const ContentsContainer = styled.div`
-  width: 100%;
-`;
-
-ContentsContainer.displayName = 'ContentsContainer';

7b37d9f9d0451f3240e85d8d7e9a1ddf2174da7a
Vitali Akulau <jerimi2008@tut.by>
2021-05-07 13:36:23 +0300

    add users.js test data

---
+++ client/cypress/fixtures/users.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
+export const validUser = {
+  name: 'valid_user',
+  email: 'test@test.com',
+  password: '123456',
+  confirmPassword: '123456',
+};
